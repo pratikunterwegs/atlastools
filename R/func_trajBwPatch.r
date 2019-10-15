9be7124fe89c36ab475419641d52afd2796b355c
Pratik Gupte <pratikgupte16@gmail.com>
2019-10-15 13:42:25 +0200

    streamline and fix trajPatch func

--- R/func_trajBwPatch.r
+++ R/func_trajBwPatch.r
@@ -15,26 +15,27 @@ funcPatchTraj <- function(df, x1 = "X_end", x2 = "X_start",
 
   # select cols from dfs
   {
-    x1 <- pull(df, x1)
-    x2 <- pull(df, x2)
+    x1 <- df[,x1]
+    x2 <- df[,x2]
     x1 <- x1[1:length(x1)-1];
     x2 <- x2[2:length(x2)]}
   {
-    y1 <- pull(df, y1)
-    y2 <- pull(df, y2)
+    y1 <- df[,y1]
+    y2 <- df[,y2]
     y1 <- y1[1:length(y1)-1];
     y2 <- y2[2:length(y2)]
   }
   # make temptib
-  tempTib = tibble(x1,y1,x2,y2)
+  tempTib <- tibble::tibble(x1,y1,x2,y2)
   # add matrix as list col
-  tempTib = tempTib %>%
-    mutate(ptsMat = pmap(tempTib, function(x1,x2,y1,y2){
-      matrix(c(x1,y1,x2,y2), ncol = 2, byrow = T)
-    }))
+  tempTib <- dplyr::mutate(tempTib, ptsMat = purrr::pmap(tempTib, function(x1,x2,y1,y2)
+  {
+    m <- matrix(c(x1,y1,x2,y2), ncol = 2, byrow = T)
+    return(m)
+  }))
 
   # make multilinestring from list col
-  ml = st_multilinestring(tempTib$ptsMat)
+  ml <- sf::st_multilinestring(tempTib$ptsMat)
 
   # return ml obj
   return(ml)

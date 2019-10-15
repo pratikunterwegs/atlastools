c8a1c4fa6ad95a21a791a4d7d0c8d79220b4b512
Pratik Gupte <pratikgupte16@gmail.com>
2019-10-15 11:53:34 +0200

    remove pipes from getResPatch

--- R/func_residencePatch.r
+++ R/func_residencePatch.r
@@ -33,192 +33,199 @@ funcGetResPatches <- function(df, x = "x", y = "y", time = "time",
   tryCatch(
     {
       # convert to sf points object
-      pts = df %>%
-        dplyr::group_by(id, tidalcycle, resPatch, type) %>%
-        tidyr::nest() %>%
-        # make sd
-        dplyr::mutate(sfdata = purrr::map(.$data, function(dff){
-          sf::st_as_sf(dff, coords = c("x", "y")) %>%
-            # assign crs
-            sf::`st_crs<-`(32631)}))#
+      pts <- dplyr::group_by(df, id, tidalcycle, resPatch, type)
+      pts <- tidyr::nest(pts)
+      pts <- dplyr::mutate(pts, sfdata = purrr::map(.$data, function(dff)
+      {
+        dff <- sf::st_as_sf(dff, coords = c("x", "y"))
+        # set crs to UTM 31N
+        sf::st_crs(dff) <- 32631
+        return(dff)
+      }))
 
       # make polygons
-      pts = pts %>%
-        dplyr::mutate(polygons = purrr::map(.$sfdata, function(dff){
-          # draw a 10 m buffer (arbitrary choice)
-          sf::st_buffer(dff, buffsize) %>%
-            dplyr::summarise()})) %>%
-        # remove sf data
-        dplyr::select(-sfdata)
+      pts <- dplyr::mutate(pts, polygons = purrr::map(.$sfdata, function(dff)
+      {
+        # draw a 10 m buffer (arbitrary choice)
+        dfpolygon <- sf::st_buffer(dff, buffsize)
+        dfpolygon <- dplyr::summarise(dfpolygon)
+
+        return(dfpolygon)
+      }))
+
+      # remove sf data
+      pts <- dplyr::select(pts, -sfdata)
 
       # remove point polygons here, MIN polygon size is 5 points
-      pts = pts %>%
-        dplyr::filter(purrr::map_int(data, nrow) > 5)
+      pts <- dplyr::filter(pts, purrr::map_int(data, nrow) > 5)
 
       # cast all to multipolygon and then single polygon
-      pts = pts %>%
-        dplyr::mutate(polygons = purrr::map(polygons, function(dff){
-          sf::st_cast(dff, "MULTIPOLYGON") %>%
-            sf::st_cast(., "POLYGON") %>%
-            # get area for later filtering
-            dplyr::mutate(area = as.numeric(st_area(.))) #%>%
+      pts <- dplyr::mutate(pts, polygons = purrr::map(polygons, function(dff)
+      {
+        dff <- sf::st_cast(dff, "MULTIPOLYGON")
+        dff <- sf::st_cast(dff, "POLYGON")
 
-        })) %>%
-        # remove point polygons if real
-        # keeps inferred polygons, which have only a single coord
-        dplyr::mutate(polygons = purrr::map2(polygons, type, function(spatial, kind){
-          if(kind == "real"){
-            spatial = spatial %>%
-              dplyr::filter(area > 100*pi)
-          }
-          return(spatial)
-        }))
+        # get area for later filtering
+        dff <- dplyr::mutate(dff, area = as.numeric(sf::st_area(.)))
 
-      # return to summarising residence patch data from points
-      patchSummary = pts %>%
-        # add ungroup, dplyr version changes have an effect
-        dplyr::ungroup() %>%
-        dplyr::transmute(id = id,
-                  tidalcycle = tidalcycle,
-                  type = type,
-                  resPatch = resPatch,
-                  summary = purrr::map(data, function(df){
-                    # arrange by time
-                    dff <- dplyr::arrange(df, time)
+      }))
 
-                    # get summary of time to determine merging based on temporal proximity
-                    dff <- dff %>%
-                      dplyr::summarise_at(vars(time),
-                                   list(start = first,
-                                        mean = mean))
+      # remove point polygons if real, keeping inferred polygons, which have only a single coord
+      pts <- dplyr::mutate(polygons = purrr::map2(polygons, type, function(spatial, kind)
+      {
+        if(kind == "real"){
+          spatial <- dplyr::filter(spatial, area > 100*pi)
+        }
+        return(spatial)
+      }))
 
-                    # return this
-                    return(dff)
-
-                  })) %>%
-        # unnest the data
-        tidyr::unnest(cols = c("summary")) %>%
-        # arrange in order of start time for interpatch distances
-        dplyr::arrange(start) %>%
-        # needs ungrouping
-        dplyr::ungroup(.)
+      # summarise residence patch data from points
+      # add ungroup, dplyr version changes have an effect
+      patchSummary <- dplyr::ungroup(pts)
+      patchSummary <- dplyr::transmute(patchSummary,
+                                       id = id,
+                                       tidalcycle = tidalcycle,
+                                       type = type,
+                                       resPatch = resPatch,
+                                       summary = purrr::map(data, function(df)
+                                       {
+                                         # arrange by time
+                                         dff <- dplyr::arrange(df, time)
+                                         # get summary of time to determine merging based on temporal proximity
+                                         dff <- dplyr::summarise_at(dff, dplyr::vars(time),
+                                                                    list(start = dplyr::first,
+                                                                         mean = mean))
+                                         # return this
+                                         return(dff)
+                                       }))
+      # unnest the data
+      patchSummary <- tidyr::unnest(patchSummary, cols = c("summary"))
+      # arrange in order of start time for interpatch distances
+      patchSummary <- dplyr::arrange(patchSummary, start)
+      # needs ungrouping
+      patchSummary <- dplyr::ungroup(patchSummary)
 
       # join summary data with spatial data
-      pts = dplyr::left_join(pts, patchSummary)
+      pts <- dplyr::left_join(pts, patchSummary)
 
       # unnest polygons column to get data
       # this has issues because of incompatible data types - check sf/dplyr/tidyr version
-      pts = pts %>%
-        tidyr::unnest_legacy(cols = c(polygons), .drop = FALSE) %>%
-        # remove polygons col, which is now geometry
-        dplyr::select(-polygons)
+      pts <- tidyr::unnest_legacy(pts, cols = c(polygons), .drop = FALSE)
+      # remove polygons col, which is now geometry
+      pts <- dplyr::select(pts, -polygons)
 
       # clear garbage
       gc()
 
       # make actual polygons object
-      pts =
-        pts %>%
-        sf::st_as_sf(., sf_column_name = "geometry")
+      pts <- sf::st_as_sf(pts, sf_column_name = "geometry")
 
       # get distance between polygons in space and time (using MEAN TIME)
-      pts = pts %>%
-        # requires ungroup
-        dplyr::ungroup(.) %>%
-        # the spatial distance is already the edge to edge distance
-        # not the centroid to centroid distance
-        dplyr::mutate(spatdiff = c(Inf, as.numeric(sf::st_distance(x = pts[1:nrow(pts)-1,],
-                                                        y = pts[2:nrow(pts),],
-                                                        by_element = T))),
-               # temporal diffs on mean
-               timediff = c(Inf, diff(mean)))
+      pts <- dplyr::ungroup(pts)
+      # the spatial distance is already the edge to edge distance
+      # not the centroid to centroid distance
+      pts <- dplyr::mutate(pts, spatdiff = c(Inf, as.numeric(sf::st_distance(x = pts[1:nrow(pts)-1,],
+                                                                             y = pts[2:nrow(pts),],
+                                                                             by_element = T))),
+                           # temporal diffs on mean
+                           timediff = c(Inf, diff(mean)))
 
       # identify independent patches
-      pts = pts %>%
-        dplyr::mutate(indePatch = cumsum(timediff > 3600 | spatdiff > 50))
+      pts <- dplyr::mutate(pts, indePatch = cumsum(timediff > 3600 | spatdiff > 50))
 
       # merge polygons by indepatch and handle the underlying data
-      pts =
-        pts %>%
-        sf::`st_crs<-`(32631) %>%
-        dplyr::group_by(id, tidalcycle, indePatch) %>%
-        # merge polygons
-        dplyr::summarise(data = list(data),
-                  # tag patches as mixed if comprised of real and inferred
-                  type = ifelse(length(unique(type)) == 2,
-                                "mixed", dplyr::first(type))) %>%
-        # get the distinct observations
-        dplyr::mutate(data = map(data, function(dff){
-          dff %>%
-            dplyr::bind_rows() %>%
-            dplyr::distinct() %>%
-            dplyr::arrange(time) %>%
-            sf::st_as_sf(coords = c("x", "y")) %>%
-            sf::`st_crs<-`(32631)
-        })) %>%
-        mutate(data = purrr::map2(data, geometry, function(dff1, dff2){
-          # keep rows contained by polygon
-          dff1 = dff1[unlist(sf::st_contains(dff2, dff1)),]
-          # return kept rows
-          return(dff1)
-        }))
+      sf::st_crs(pts) <- 32631 # setting crs to 31N
+      pts <- dplyr::group_by(pts, id, tidalcycle, indePatch)
+      # merge polygons
+      pts <- dplyr::summarise(pts, data = list(data),
+                              # tag patches as mixed if comprised of real and inferred
+                              type = ifelse(length(unique(type)) == 2,
+                                            "mixed", dplyr::first(type)))
+      # get the distinct observations
+      pts <- dplyr::mutate(pts, data = map(data, function(dff)
+      {
+        dff <- dplyr::bind_rows(dff)
+        dff <- dplyr::distinct(dff)
+        dff <- dplyr::arrange(df, time)
+        dff <- sf::st_as_sf(dff, coords = c("x", "y"))
+        sf::st_crs(dff) <- 32631 # setting to UTM 31N
+
+        return(dff)
+      }))
+
+      # clip underlying data by polygon boundaries
+      pts <- mutate(pts, data = purrr::map2(data, geometry, function(dff1, dff2)
+      {
+        # keep rows contained by polygon
+        dff1 <- dff1[unlist(sf::st_contains(dff2, dff1)),]
+        # return kept rows
+        return(dff1)
+      }))
 
       # get patch summary from underlying data
-      pts =
-        pts %>%
-        # add x,y,time summary
-        dplyr::mutate(patchSummary = map(data, function(dff){
-          dff %>%
-            dplyr::bind_cols(., sf::st_coordinates(dff) %>% tibble::as_tibble()) %>%
-            sf::st_drop_geometry() %>%
-            dplyr::arrange(time) %>%
-            dplyr::summarise_at(dplyr::vars(X, Y, time, tidaltime),
-                         list(mean = mean,
-                              start = first,
-                              end = last))
-        })) %>%
-        # add total within patch distance
-        dplyr::mutate(distInPatch = purrr::map_dbl(data, function(dff){
-          sum(dff$dist, na.rm = T) # this distance is calculated earlier in the processing
-        }))
+      # add x,y,time summary
+      pts <- dplyr::mutate(pts, patchSummary = map(data, function(dff)
+      {
+        # get temporary coordinates before dropping geometry
+        tempcoords <- sf::st_coordinates(dff)
+        tempcoords <- tibble::as_tibble(tempcoords)
+
+        # add coords to dataframe
+        dff <- dplyr::bind_cols(dff, tempcoords)
+        dff <- sf::st_drop_geometry(dff)
+        dff <- dplyr::arrange(dff, time)
+        dff <- dplyr::summarise_at(dff, dplyr::vars(X, Y, time, tidaltime),
+                                   list(mean = mean,
+                                        start = dplyr::first,
+                                        end = dplyr::last))
+
+        return(dff)
+      }))
+
+      # add total within patch distance
+      pts <- dplyr::mutate(pts, distInPatch = purrr::map_dbl(data, function(dff)
+      {
+        return(sum(dff$dist, na.rm = T)) # this distance is calculated earlier in the processing
+      }))
 
       # export sf object if requested
       if(returnSf == TRUE){
-        patchSf = pts %>%
-          dplyr::mutate(time_mean = purrr::map_dbl(patchSummary, function(thisdata){
-            thisdata$time_mean
-          })) %>%
-          dplyr::select(id, tidalcycle, time_mean,
+        # add mean time
+        patchSf <- dplyr::mutate(pts, time_mean = purrr::map_dbl(patchSummary, function(thisdata)
+        {
+          return(thisdata$time_mean)
+        }))
+        # select some vars
+        patchSf <- dplyr::select(patchSf, id, tidalcycle, time_mean,
                                  patch = indePatch, geometry,
-                                 time_mean, type) %>%
-          # hopefully fixes some patch misordering
-          dplyr::arrange(time_mean) %>%
-          dplyr::mutate(patch = 1:nrow(.))
+                                 time_mean, type)
+        # reorder and renumber patches by time
+        patchSf <- dplyr::arrange(patchSf, time_mean)
+        patchSf <- dplyr::mutate(patchSf, patch = 1:nrow(.))
       }
 
 
       # arrange patches by start time and add between patch distance
-      pts =
-        pts %>%
-        # add area and number of fixes and distance per patch
-        # also proportion of expected positions received
-        dplyr::mutate(area = as.numeric(sf::st_area(.)),
-               nfixes = purrr::map_int(data, nrow),
-               distPerPoint = distInPatch/nfixes) %>%
+      # add area and number of fixes and distance per patch
+      # also proportion of expected positions received
 
+      pts <- dplyr::mutate(pts, area = as.numeric(sf::st_area(.)),
+                           nfixes = purrr::map_int(data, nrow),
+                           distPerPoint = distInPatch/nfixes)
         # drop geometry
-        sf::st_drop_geometry() %>%
-        # remove data column
-        dplyr::select(-data) %>%
-        tidyr::unnest_legacy(cols = c(patchSummary), .drop = TRUE) %>%
-        dplyr::arrange(time_mean) %>%
+        pts <- sf::st_drop_geometry(pts)
+        # remove data column, unnest summary and arrange by time
+        pts <- dplyr::select(pts, -data)
+        pts <- tidyr::unnest_legacy(pts, cols = c(patchSummary), .drop = TRUE)
+        pts <- dplyr::arrange(pts, time_mean)
+
         # add distance between and duration in SECONDS
-        dplyr::mutate(patch = 1:nrow(.),
-               distBwPatch = funcPatchDistance(., x1 = "X_end", x2 = "X_start",
-                                               y1 = "Y_end", y2 = "Y_start"),
-               duration = time_end - time_start,
-               propFixes = nfixes/(duration/3)) %>%
-        dplyr::select(-indePatch)
+        pts <- dplyr::mutate(pts, patch = 1:nrow(.),
+                      distBwPatch = funcPatchDistance(., x1 = "X_end", x2 = "X_start",
+                                                      y1 = "Y_end", y2 = "Y_start"),
+                      duration = time_end - time_start,
+                      propFixes = nfixes/(duration/3))
+        pts <- dplyr::select(pts, -indePatch)
 
       gc();
 

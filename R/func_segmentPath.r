7d025fd8ab01aa96c5fab0f002c6d2f2f4ec25f5
pratikunterwegs <pratikgupte16@gmail.com>
2019-10-16 15:54:20 +0200

    correct df comaprison

--- R/func_segmentPath.r
+++ R/func_segmentPath.r
@@ -87,7 +87,8 @@ funcSegPath <- function(revdata, resTimeLimit = 2, travelSeg = 5,
     # add type to real data
     df[,type:="real"]
 
-    if(nrow(tempdf < 2))
+    # enter this step only if there are 2 or more rows of data between which to infer patches
+    if(nrow(tempdf >= 2))
     {
       print(glue::glue('\n {unique(tempdf$id)} has no inferred data'))
       # make list column of expected times with 3 second interval

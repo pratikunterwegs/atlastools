ae969969c62ab2544e55562ba1de7f6bdc7c1911
pratik <p.r.gupte@rug.nl>
2019-11-04 18:55:24 +0100

    remove useless args

--- R/func_classifyPath.r
+++ R/func_classifyPath.r
@@ -1,15 +1,9 @@
-#' segPath2
+#' classifyPath
 #'
-#' @param revdata A string filepath to data in csv format. The data must be the output of recurse analysis, or must include, in addition to X, Y and time columns, a residence time column named resTime, id, and tidalcycle.
 #' @param resTimeLimit A numeric giving the time limit in minutes against which residence time is compared.
+#' @param somedata A data frame which must have a column for the residence time at each point.
+#' @param restimeCol The residence time column name.
 #' @param travelSeg A numeric value of the number of fixes, or rows, over which a smoother function is applied.
-#' @param inferPatches A logical indicating whether residence patches should be inferred from temporal gaps in the data.
-#' @param infPatchTimeDiff A numeric duration in seconds, of the minimum time difference between two points, above which, it is assumed worthwhile to examine whether there is a missing residence patch to be inferred.
-#' @param infPatchSpatDiff A numeric distance in metres, of the maximum spatial distance between two points, below which it may be assumed few extreme movements took place between them.
-#' @param htdata A string filepath to data in csv format. The data must be the output of tidal cycle finding analysis, or must include, in addition to X, Y and time columns, a tidaltime column named tidaltime; also id, and tidalcycle for merging.
-#' @param bufferSize A numeric value, in meteres, of the size of the buffer to be constructed around points.
-#' @param resPatchTimeDiff A numeric value, in seconds, of the time between two patches for them to be considered independent.
-#' @param resPatchSpatDiff Anumeric value, in metres, of the distance between two patches for them to be considered independent.
 #'
 #' @return A data.frame extension object. This dataframe has the added column \code{resPatch} based on cumulative patch summing. Depending on whether \code{inferPatches = TRUE}, the dataframe has additional inferred points. An additional column is created in each case, indicating whether the data are empirical fixes ('real') or 'inferred'.
 #' @import data.table
@@ -68,26 +62,6 @@ funcClassifyPath <- function(somedata,
              # drop NAs in rolling residence time evaluation
              # essentially the first and last elements will be dropped
   somedata <- somedata[!is.na(rollResTime) & resTimeBool == T, ]
-
-  # assign spat and temp diff columns
-  somedata <- somedata[,`:=`(spatdiff = watlasUtils::funcDistance(somedata = somedata, x = "x", y = "y"),
-                             tempdiff = as.numeric(c(NA, diff(time))))]
-
-  # first spatial difference is infinity for calculation purposes
-  somedata[1,c("spatdiff", "tempdiff")] <- Inf
-
-  # merge points if not spatially or temporally independent
-  # compare distance from previous point to buffersize
-  somedata <- somedata[,patch := cumsum(spatdiff > (2*resPatchSpatDiff) &
-                                          tempdiff > resPatchTimeDiff ) ]
-
-  # count number of points per patch
-  somedata <- somedata[,nfixes := .N, by = c("id", "tidalcycle", "patch")]
-
-  # remove patches with 5 or fewer points
-  somedata[nfixes > 5, ]
-
-  #
 
   # remove useless somedata columns
   data.table::set(somedata, ,c("rollResTime", "resTimeBool"), NULL)

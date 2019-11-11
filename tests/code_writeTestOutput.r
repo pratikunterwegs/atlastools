725cfcfaee212022d631b001c66a6cb171f194ba
pratikunterwegs <pratikgupte16@gmail.com>
2019-11-11 11:46:20 +0100

    basic tested data processing

--- tests/code_writeTestOutput.r
+++ tests/code_writeTestOutput.r
@@ -9,3 +9,16 @@ library(ggplot2)
 data_files <- list.files("tests/testdata/recdata/", pattern = "recurse", full.names = TRUE)
 tide_files <- list.files("tests/testdata/htdata/", full.names = TRUE)
 
+#### infer residence ####
+this_data <- list(data_files, tide_files)
+this_data <- purrr::map(this_data, purrr::map, readr::read_csv)
+this_data <- purrr::map2(this_data[[1]], this_data[[2]], watlasUtils::funcInferResidence)
+
+#### classify path ####
+classified_data <- purrr::map(this_data, watlasUtils::funcClassifyPath)
+
+#### get residence patch sf objects ####
+patch_data <- purrr::map(classified_data, funcGetResPatch, makeSf = TRUE)
+
+#### combine sf objects and write to shp ####
+patch_sum <- purrr::reduce(patch_data, dplyr::bind_rows)

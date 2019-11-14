30e06f43ae4d3989a32f5d5d28a6560e2c10f8be
pratik <p.r.gupte@rug.nl>
2019-11-14 17:23:28 +0100

    change layout

--- inst/shiny-examples/tweak_patch_param/ui.r
+++ inst/shiny-examples/tweak_patch_param/ui.r
@@ -4,52 +4,61 @@ library(shiny)
 ui <- fluidPage(
     titlePanel("WATLAS Utilities App"),
 
-    sidebarLayout(
-        sidebarPanel(
-            h1("Function parameters"),
-            fileInput("revfile", p("recurse data")),
-            fileInput("htfile", p("tide data")),
+    fluidRow(
+        column(3,
+               plotOutput("patch_map")
+        ),
+        column(3,
+               plotOutput("resTime_time")
+        ),
+        column(3,
+               tableOutput("patchSummary"))
+    ),
+    hr(),
 
-
-            p("inferResidence: inferring residence points"),
-            numericInput("infResTime",
-                p("res time inferred pts (min)"),
-                value = 2.0),
-            numericInput("infPatchTimeDiff",
-                p("min time gap missing data (min)"),
-                value = 2.0),
-            numericInput("infPatchSpatDiff",
-                p("max spatial gap missing data (min)"),
-                value = 2.0),
-
-            p("classifyPath: classify as stationary"),
-            numericInput("resTimeLimit",
-                p("residence time limit (min)"),
-                value = 2.0),
-            textInput("resTimeCol",
-                p("residence time column"),
-                value = "resTime"),
-            numericInput("travelSeg",
-                p("travel segment length (fixes)"),
-                value = 5.0),
-
-            p("getPatches: construct residence patches"),
-            numericInput("bufferSize",
-                p("spatial buffer size (m)"),
-                value = 10.0),
-            numericInput("tempIndepLimit",
-                p("temp indep limit (s)"),
-                value = 1800),
-            numericInput("spatIndepLimit",
-                p("spat indep limit (m)"),
-                value = 100.0),
-            actionButton(inputId = "go",
-                label = "Nuke data")
-            ),
-
-        mainPanel(
-            plotOutput("everything")
-            )
+    fluidRow(
+        column(2,
+               h4("Patch construction parameters"),
+               fileInput("revfile", p("recurse data")),
+               fileInput("htfile", p("tide data")),
+        ),
+        column(3,
+               h4("inferResidence: inferring residence points"),
+               numericInput("infResTime",
+                            p("res time inferred pts (min)"),
+                            value = 2.0),
+               numericInput("infPatchTimeDiff",
+                            p("min time gap missing data (min)"),
+                            value = 2.0),
+               numericInput("infPatchSpatDiff",
+                            p("max spatial gap missing data (min)"),
+                            value = 2.0)
+        ),
+        column(3,
+               h4("classifyPath: classify as stationary"),
+               numericInput("resTimeLimit",
+                            p("residence time limit (min)"),
+                            value = 2.0),
+               textInput("resTimeCol",
+                         p("residence time column"),
+                         value = "resTime"),
+               numericInput("travelSeg",
+                            p("travel segment length (fixes)"),
+                            value = 5.0),
+        ),
+        column(3,
+               h4("getPatches: construct residence patches"),
+               numericInput("bufferSize",
+                            p("spatial buffer size (m)"),
+                            value = 10.0),
+               numericInput("tempIndepLimit",
+                            p("temp indep limit (s)"),
+                            value = 1800),
+               numericInput("spatIndepLimit",
+                            p("spat indep limit (m)"),
+                            value = 100.0),
+               actionButton(inputId = "go",
+                            label = "Nuke data")
         )
-
     )
+)

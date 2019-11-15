f0a7ffe2f81bc8a92bff0cb2dad4b6c2ef2575c6
Pratik Gupte <pratikgupte16@gmail.com>
2019-11-15 19:38:00 +0100

    add raw data to plots

--- inst/shiny-examples/tweak_patch_param/ui.r
+++ inst/shiny-examples/tweak_patch_param/ui.r
@@ -1,61 +1,75 @@
-# load packages
+# ui function
 library(shiny)
 library(shinythemes)
 
 ui <- fluidPage(
-    titlePanel("WATLAS Utilities App"),
+  titlePanel("WATLAS Utilities App"),
 
-    fluidRow(
-        column(5,
-               h4("Residence patches"),
-               textOutput("map_label"),
-               plotOutput("patch_map", height = "600px")),
-        column(3,
-               h4("Residence time ~ time"),
-               textOutput("map_label"),
-               plotOutput("resTime_time",
-                          height = "300px", width = "600px"),
-               h4("Patch summary"),
-               tableOutput("patchSummary"))),
-    hr(),
-    fluidRow(
-        column(2,
-               h4("Patch construction data"),
-               fileInput("revfile", p("recurse data")),
-               fileInput("htfile", p("tide data")),
-               actionButton(inputId = "go",
-                            label = "Make patches")),
-        column(2,
-               h4("inferResidence: inferring residence points"),
-               numericInput("infResTime",
-                            p("res time inferred pts (min)"),
-                            value = 2.0),
-               numericInput("infPatchTimeDiff",
-                            p("min time gap missing data (s)"),
-                            value = 1800.0),
-               numericInput("infPatchSpatDiff",
-                            p("max spatial gap missing data (m)"),
-                            value = 100.0)),
-        column(2,
-               h4("classifyPath: classify as stationary"),
-               numericInput("resTimeLimit",
-                            p("residence time limit (min)"),
-                            value = 2.0),
-               textInput("resTimeCol",
-                         p("residence time column"),
-                         value = "resTime"),
-               numericInput("travelSeg",
-                            p("travel segment length (fixes)"),
-                            value = 5.0)),
-        column(2,
-               h4("getPatches: construct residence patches"),
-               numericInput("bufferSize",
-                            p("spatial buffer size (m)"),
-                            value = 10.0),
-               numericInput("tempIndepLimit",
-                            p("temp indep limit (s)"),
-                            value = 1800),
-               numericInput("spatIndepLimit",
-                            p("spat indep limit (m)"),
-                            value = 100.0)))
-)
+  fluidRow(
+    column(5,
+      h4("Residence patches"),
+      # textOutput("this_map_label"),
+      plotOutput("patch_map", height = "600px")
+      ),
+
+    column(3,
+     h4("Residence time ~ time"),
+     # textOutput("this_map_label"),
+     plotOutput("resTime_time", height = "300px", width = "800px"),
+     h4("Patch summary"),
+     tableOutput("patchSummary")
+     )
+    ),
+  hr(),
+
+  fluidRow(
+    column(2,
+     h4("Patch construction data"),
+     fileInput("revfile", p("Recurse data")),
+     fileInput("htfile", p("Tide data")),
+     actionButton(inputId = "go", label = "Run")
+     ),
+
+    column(2,
+     # h4("inferResidence: inferring residence points"),
+     numericInput("infResTime",
+      p("res time inferred pts (min)"),
+      value = 2.0),
+     numericInput("infPatchTimeDiff",
+      p("min time gap missing data (s)"),
+      value = 1800.0),
+     numericInput("infPatchSpatDiff",
+      p("max spatial gap missing data (m)"),
+      value = 100.0)
+     ),
+
+    column(2,
+     # h4("classifyPath: classify as stationary"),
+     numericInput("resTimeLimit",
+      p("residence time limit (min)"),
+      value = 2.0),
+     textInput("resTimeCol",
+       p("residence time column"),
+       value = "resTime"),
+     numericInput("travelSeg",
+      p("travel segment length (fixes)"),
+      value = 5.0)
+     ),
+
+    column(2,
+     # h4("getPatches: construct residence patches"),
+     numericInput("bufferSize",
+      p("spatial buffer size (m)"),
+      value = 10.0),
+     numericInput("tempIndepLimit",
+      p("temp indep limit (s)"),
+      value = 1800),
+     numericInput("spatIndepLimit",
+      p("spat indep limit (m)"),
+      value = 100.0)
+     )
+    )
+  )
+    # theme = shinytheme("yeti")
+  #   )
+  # )

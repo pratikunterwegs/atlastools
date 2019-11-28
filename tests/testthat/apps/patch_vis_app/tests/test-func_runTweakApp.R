1da27569062e18d26aa10cde8b05f4ba7bef4c2b
pratik <p.r.gupte@rug.nl>
2019-11-28 18:32:43 +0100

    simplify tests and remove examples

--- tests/testthat/apps/patch_vis_app/tests/test-func_runTweakApp.R
+++ tests/testthat/apps/patch_vis_app/tests/test-func_runTweakApp.R
@@ -2,8 +2,5 @@ app <- ShinyDriver$new("../")
 app$snapshotInit("test-func_runTweakApp", screenshot=TRUE)
 
 app$listWidgets()
-    # app$uploadFile(revfile = "../../../../tests/testdata/recdata/recurse435_008.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
-    # app$uploadFile(htfile = "../../../../tests/testdata/htdata/435_008.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
-    # app$setInputs(go = "click")
 app$snapshot(items = list(output = FALSE, export = FALSE, input = TRUE))
-    # # app$snapshot(items = list(output = "patchSummary"))
+

25fe9f29c8650b9e967aa54f1c46c4e83faa8025
pratik <p.r.gupte@rug.nl>
2020-03-04 14:38:06 +0100

    restr testdata maker

--- tests/testdata/make_testData.r
+++ tests/testdata/make_testData.r
@@ -3,7 +3,7 @@
 resTimeLimit = 4
 
 testrevdata = tibble::tibble(id = 4e3,
-	tidalcycle = 1e3,
+	tide_number = 1e3,
 	x = rep(c(seq(0, 5e3, 1e3),
 	          seq(5e3, 0, -1e3)), each = 100) +
 	  runif(1200, 0, 2e1),
@@ -11,16 +11,11 @@ testrevdata = tibble::tibble(id = 4e3,
 	  runif(1200, 0, 2e1),
 	time = c(seq(1, 1e4, length.out = length(x))),
 	resTime = c(rep(resTimeLimit, 550), rep(2, 100),
-							rep(resTimeLimit, 550)))
-
-# create ht data
-testhtdata = tibble::tibble(id = 4e3,
-	tidalcycle = 1e3,
-	time = c(seq(1, 1e4, length.out = nrow(testrevdata))),
-	tidaltime = seq(1, 12.41*60, length.out = nrow(testrevdata)))
+							rep(resTimeLimit, 550)),
+	tidaltime = seq(1, 12.41*60, length.out = length(resTime)),
+	waterlevel = cospi(pi*seq(1, 12.41*60, length.out = length(tidaltime))/(11*60*2))*100)
 
 # write to file for testing
-readr::write_csv(testrevdata, "../watlasUtils/tests/testdata/test_revdata.csv")
-readr::write_csv(testhtdata, "../watlasUtils/tests/testdata/test_htdata.csv")
+readr::write_csv(testrevdata, "tests/testdata/test_revdata.csv")
 
 # ends here

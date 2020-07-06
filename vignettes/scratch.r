e821ec96b475378d831e76e81a0e80936efe0680
P.R. Gupte <p.r.gupte@rug.nl>
2020-07-06 16:13:51 +0200

    add vignettes for what it's worth

--- vignettes/scratch.r
+++ vignettes/scratch.r
@@ -1,12 +1,12 @@
 ## Getting raw data
 
-`atlastools` has the function `wat_get_data` Gets data from the NIOZ server.
-`wat_get_data` is untested because the data are stored on the NIOZ servers. I haven't worked out whether connectting and retrieving this data is actually possible. An example is provided, but will not work, because the data access arguments are not public.
+`atlastools` has the function `atl_get_data` Gets data from the NIOZ server.
+`atl_get_data` is untested because the data are stored on the NIOZ servers. I haven't worked out whether connectting and retrieving this data is actually possible. An example is provided, but will not work, because the data access arguments are not public.
 
 ```{r get_raw_data}
 # get an example raw data file
 
-prelim_data <- wat_get_data(tag = 435,
+prelim_data <- atl_get_data(tag = 435,
                             tracking_time_start = "<release_date_435>",
                             tracking_time_end = "2018-10-31",
                             username = "<database_username>",
@@ -31,7 +31,7 @@ We first remove any points lying inside the region of attractor points.
 # here we remove attractor points which we have already identified
 attractor_points <- fread("../private/attractor_points.txt")
 
-temp_data <- wat_rm_attractor(df = raw_data,
+temp_data <- atl_rm_attractor(df = raw_data,
                               atp_xmin = atp$xmin,
                               atp_xmax = atp$xmax,
                               atp$ymin = atp$ymin,
@@ -42,7 +42,7 @@ Now we clean the data by applying a median filter, removing data with too few ba
 
 ```{r clean_data}
 # here we clean the data
-clean_data <- wat_clean_data(somedata = temp_data,
+clean_data <- atl_clean_data(somedata = temp_data,
                              moving_window = 3,
                              nbs_min = 0,
                              sd_threshold = 100,
@@ -52,11 +52,11 @@ clean_data <- wat_clean_data(somedata = temp_data,
 
 ## Aggregating data
 
-For data quality and computational reasons, we might want to aggregate the data over a specific interval. Here, we use a 30 second interval for aggregation. This is done using `wat_agg_data`.
+For data quality and computational reasons, we might want to aggregate the data over a specific interval. Here, we use a 30 second interval for aggregation. This is done using `atl_agg_data`.
 
 ```{r clean_raw_data}
 # aggregate over 30 seconds
-agg_data <- wat_agg_data(df = clean_data, interval = 30)
+agg_data <- atl_agg_data(df = clean_data, interval = 30)
 ```
 
 # Adding covariates
@@ -64,11 +64,11 @@ agg_data <- wat_agg_data(df = clean_data, interval = 30)
 ## Adding tidal cycles
 
 We want to eventually split the full track into subsets, these subsets to be separated by high tide.
-This means adding the time of high tide to the data using `wat_add_tide`,
+This means adding the time of high tide to the data using `atl_add_tide`,
 The function needs a data frame of high tide times; this is not covered here.
 
 ```{r add_tide_data}
 # add tide data
-agg_data <- wat_add_tide(df = agg_data,
+agg_data <- atl_add_tide(df = agg_data,
                          tide_data = "../private/")
 ```

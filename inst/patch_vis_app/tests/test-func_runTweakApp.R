78dae12f3be8c8c3f327aa2a8a7cafc8464d2517
P.R. Gupte <p.r.gupte@rug.nl>
2020-07-06 11:42:42 +0200

    remove damn shiny app

--- inst/patch_vis_app/tests/test-func_runTweakApp.R
+++
@@ -1,3 +0,0 @@
-app <- shinytest::ShinyDriver$new("../")
-app$snapshotInit("test-func_wat_run_patch_vis_app", screenshot = FALSE)
-app$snapshot(items = list(output = FALSE, export = FALSE, input = TRUE))

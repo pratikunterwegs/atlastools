345f1534f05d977a8113eb588adc1c6cc1ee9aa8
P.R. Gupte <p.r.gupte@rug.nl>
2020-08-03 14:40:48 +0200

    changes to refl logic

--- R/fun_remove_reflections.R
+++ R/fun_remove_reflections.R
@@ -19,7 +19,7 @@ atl_remove_reflections <- function(data,
                                    y = "y",
                                    time = "time",
                                    point_angle_cutoff = 45,
-                                   reflection_speed_cutoff = 40,
+                                   reflection_speed_cutoff = 20,
                                    est_ref_len = 1000) {
   speed <- NULL
   # check data
@@ -41,7 +41,7 @@ atl_remove_reflections <- function(data,
                           data$angle >= point_angle_cutoff)[1] - 1
 
   # message
-  message(glue::glue("first anchor at {anchor_point}"))
+  # message(glue::glue("first anchor at {anchor_point}"))
 
   while (anchor_point < nrow(data) - 1) {
 
@@ -50,26 +50,24 @@ atl_remove_reflections <- function(data,
     # find the max speed after the first anomaly, which is the blink away
     # the next highest should be the blink back
     suspect_speeds <- data[(suspect_point + 1):est_ref_len, speed]
+    
+    # drop NA here
+    suspect_speeds <- stats::na.omit(suspect_speeds)
 
-    # get the next highest speed, may be higher
-    # use gt not gte else will get first suspect speed
+    # get the next highest speed
     nx_high_speed <- which.max(rank(suspect_speeds))
     # this gets the next highest speed, which should be the end of the
     # reflection, but may also be the beginning or end of another reflection
-    
-    reflection_end <- anchor_point + nx_high_speed + 1
-
-    # when reflections do not end remove all subsequent data
-    # this is more likely in smaller subsets
-    if (is.na(reflection_end)) {
-       reflection_end <- nrow(data)
+    if (suspect_speeds[nx_high_speed] < reflection_speed_cutoff) {
+      reflection_end <- nrow(data)
+      message(glue::glue("remove_reflections: reflection does not end within \\
+              {est_ref_len} positions"))
+    } else {
+      reflection_end <- suspect_point + nx_high_speed + 1 # one added for safety
     }
-
-    assertthat::assert_that(!is.na(reflection_end),
-      msg = "remove_reflections: reflection end is NA")
-
+    
     # message ref end
-    message(glue::glue("reflection ends {reflection_end}"))
+    # message(glue::glue("reflection ends {reflection_end}"))
 
     # identify rows to remove
     # may be excessive but works
@@ -79,7 +77,7 @@ atl_remove_reflections <- function(data,
     next_anchor <- which(data$speed[reflection_end:nrow(data)] >=
                            reflection_speed_cutoff &
                            data$angle[reflection_end:nrow(data)] >=
-                           point_angle_cutoff)[1] - 1
+                           point_angle_cutoff)[1]
 
     if (is.na(next_anchor)) {
       # break the loop if there's no further anomalies
@@ -91,7 +89,7 @@ atl_remove_reflections <- function(data,
                             msg = glue::glue("anchor point {anchor_point} is \\
                             before reflection end {reflection_end}"))
       # message
-      message(glue::glue("next anchor is {anchor_point}"))
+      # message(glue::glue("next anchor is {anchor_point}"))
     }
   }
 

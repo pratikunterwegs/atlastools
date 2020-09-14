da264210a52da799ef1ac59d78570eb6a65feb11
P.R. Gupte <p.r.gupte@rug.nl>
2020-09-14 15:32:46 +0200

    fix before after with x, y

--- R/fun_plot_before_after.R
+++ R/fun_plot_before_after.R
@@ -4,10 +4,14 @@
 #' @param data The data passed to the pre-preprocessing function.
 #' @param fun The pre-processing function that operates on data.
 #' @param ... Arguments to the pre-processing function.
+#' @param x The X coordinate.
+#' @param y The Y coordinate.
 #'
 #' @return Nothing. Makes a plot.
 #' @export
 atl_before_after <- function(data,
+                             x = "x",
+                             y = "y",
                              fun, ...) {
   # check data
   assertthat::assert_that(is.data.frame(data),
@@ -20,15 +24,15 @@ atl_before_after <- function(data,
   
   # apply function to data COPY
   data_copy <- data.table::copy(data)
-  data_copy <- fun(data_copy, ...)
+  data_copy <- fun(data_copy, x = "x", y = "y", ...)
 
   # check function output
   assertthat::assert_that(is.data.frame(data_copy),
     msg = "before_after: processing result is not a data.frame")
   
   # get title as function arguments
-  # start from 3 because 1 and 2 are data and fun, 0 is the function name iirc
-  argument_values <- as.list(match.call()[3:length(match.call())])
+  # start from 5 because 1 is atl_before_after, 2, 3, 4 are data, x, and y
+  argument_values <- as.list(match.call()[5:length(match.call())])
   plot_title <- glue::glue('{names(argument_values[1])} = \\
                                           {argument_values[1]}')
   plot_subtitle <- stringr::str_c(glue::glue('{names(argument_values[-1])} = \\
@@ -36,11 +40,11 @@ atl_before_after <- function(data,
                                   collapse = "; ")
   # plot as overlay
   graphics::par(mar = rep(2, 4))
-  graphics::plot(data$x, data$y, type = "o", 
+  graphics::plot(data[[x]], data[[y]], type = "o", 
                  col = "steelblue", 
                  xaxt = "n", yaxt = "n", ann = FALSE,
                  lwd = 1, pch = 16, cex = 0.5)
-  graphics::lines(data_copy$x, data_copy$y, type = "l", 
+  graphics::lines(data_copy[[x]], data_copy[[y]], type = "l", 
                   col = "red",
                   xaxt = "n", yaxt = "n", ann = FALSE,
                   pch = 16,
@@ -49,7 +53,6 @@ atl_before_after <- function(data,
                   sub = plot_subtitle, font.sub = 3,
                   line = 0.5)
   graphics::legend(x = "bottomleft", 
-                   # y = min(data$y) + diff(range(data$y)) / 10,
                    legend = c("Raw data", "Processed data"),
                    col = c("steelblue",
                            "red"),

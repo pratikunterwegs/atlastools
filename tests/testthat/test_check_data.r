8ecd57fce3942154ffaa300bc70a3610675a4455
Pratik Gupte <pratikgupte16@gmail.com>
2020-10-15 13:49:53 +0200

    style test_check_data.r

--- tests/testthat/test_check_data.r
+++ tests/testthat/test_check_data.r
@@ -1,20 +1,26 @@
 context("check data for columns\n")
 testthat::test_that("data has expected columns", {
 
-    # make some test data
-    testdata <- data.table::data.table(x = as.double(1:1e3),
-                                       y = as.double(1:1e3),
-                                       time = as.numeric(1:1e3))
+  # make some test data
+  testdata <- data.table::data.table(
+    x = as.double(1:1e3),
+    y = as.double(1:1e3),
+    time = as.numeric(1:1e3)
+  )
 
-    # expect no error
-    testthat::expect_silent(object = {
-        atl_check_data(data = testdata,
-                                   names_expected = c("x", "y", "time"))
-    })
+  # expect no error
+  testthat::expect_silent(object = {
+    atl_check_data(
+      data = testdata,
+      names_expected = c("x", "y", "time")
+    )
+  })
 
-    # expect an error
-    testthat::expect_error(expr = {
-        atl_check_data(data = testdata,
-                                   names_expected = c("X", "Y", "TIME"))
-    })
- })
+  # expect an error
+  testthat::expect_error(expr = {
+    atl_check_data(
+      data = testdata,
+      names_expected = c("X", "Y", "TIME")
+    )
+  })
+})

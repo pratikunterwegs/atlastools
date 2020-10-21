89ff91d06a2d8f132b463ca7ec1f248acd9f1a94
Pratik Gupte <pratikgupte16@gmail.com>
2020-10-21 11:52:03 +0200

    better reflection test

--- tests/testthat/test_remove_reflections.R
+++ tests/testthat/test_remove_reflections.R
@@ -2,22 +2,32 @@ context("removing reflections")
 testthat::test_that("reflections are removed", {
 
   # make test positions
-  test_data <- data.table::fread("../testdata/simulated_data_reflection.csv")
+  test_data <- data.table::fread("../testdata/data_errors.csv")
 
   # get speeds
   test_data[, `:=`(
-    speed = atlastools::atl_get_speed(test_data),
+    in_speed = atlastools::atl_get_speed(test_data),
+    out_speed = atlastools::atl_get_speed(test_data, type = "out"),
     angle = atlastools::atl_turning_angle(test_data)
   )]
-
-  test_output <- atlastools::atl_remove_reflections(test_data)
+  
+  # remove NA speeds
+  stats::na.omit(test_data, cols = c("in_speed", "out_speed", "angle"))
+  
+  # remove outliers
+  test_data <- test_data[in_speed < 0.024 & out_speed < 0.024, ]
+  
+  # remove reflections
+  test_output <- atlastools::atl_remove_reflections(test_data,
+                                        point_angle_cutoff = 10,
+                                        reflection_speed_cutoff = 0.024)
 
   # do tests
   # should return fewer elements than nrows in df
   testthat::expect_lte(nrow(test_output), nrow(test_data))
 
   # no extreme speeds should remain
-  testthat::expect_lte(max(test_output$speed, na.rm = TRUE), 20)
+  testthat::expect_lte(max(test_output$speed, na.rm = TRUE), 0.024)
 
   # a test for change in x and y
   # make test positions
@@ -51,23 +61,29 @@ testthat::test_that("reflections are removed", {
 
 testthat::test_that("reflections do not end", {
   # make test positions
-  test_data <- data.table::fread("../testdata/simulated_data_reflection.csv")
-
-  # make an extra unending reflection
-  test_data[800:nrow(test_data), "x"] <- -20 + stats::runif(201)
-  test_data[800:nrow(test_data), "y"] <- 80 + stats::runif(201)
-
-
+  test_data <- data.table::fread("../testdata/data_errors.csv")[seq(1000L), ]
+  
   # get speeds
   test_data[, `:=`(
-    speed = atlastools::atl_get_speed(test_data),
+    in_speed = atlastools::atl_get_speed(test_data),
+    out_speed = atlastools::atl_get_speed(test_data, type = "out"),
     angle = atlastools::atl_turning_angle(test_data)
   )]
+  
+  # remove NA speeds
+  stats::na.omit(test_data, cols = c("in_speed", "out_speed", "angle"))
+  
+  # remove outliers
+  test_data <- test_data[in_speed < 0.024 & out_speed < 0.024, ]
+
+  # truncate the data so the reflection does not end
+  test_data <- test_data[seq(600), ]
 
   # now get output
   test_output <- atlastools::atl_remove_reflections(test_data,
-    reflection_speed_cutoff = 20,
-    point_angle_cutoff = 5
+    reflection_speed_cutoff = 0.024,
+    point_angle_cutoff = 5,
+    est_ref_len = 500
   )
 
   # do tests
@@ -77,5 +93,5 @@ testthat::test_that("reflections do not end", {
   # should remove points with y coordinates less than 60
   # this is still true even though the reflection does not end
   # in this subset
-  testthat::expect_lt(max(test_output$y), 60)
+  testthat::expect_lt(max(test_output$y), 1.02)
 })
